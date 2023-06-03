5460f8dfb39c2b2ef95a18bb7be9ecf97472b88e
WilliM233 <30969699+WilliM233@users.noreply.github.com>
2023-06-03 15:39:15 -0500

    finish functionality

---
+++ jamming/src/components/SearchResults.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrackList from './Tracklist';
+
+function SearchResults(props) {
+    return (
+        <div className="SearchResults">
+            <h2>Results</h2>
+            <TrackList tracks={props.searchResults} onAdd={props.onAdd} />
+        </div>
+    );
+};
+
+export default SearchResults;
